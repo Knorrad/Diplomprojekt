8e3b30fad68b1f3168672f29cadd3c6a474919b5
Konrad Lysy <konrad.lysy@hotmail.com>
2018-01-09 10:46:27 +0100

    Create User Class
    Signed-off-by: Konrad Lysy <konrad.lysy@hotmail.com>

---
+++ src/main/java/at/spengergasse/domain/User.java
@@ -0,0 +1,39 @@
+package at.spengergasse.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+/**
+ * Created by Knorrad on 09.01.2018.
+ */
+
+@Data
+@Entity
+@Table(name ="User")
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class User {
+
+    @NotNull
+    @NonNull
+    @Size(min = 3, max = 35)
+    @Column(name="username")
+    private String username;
+
+
+    @NotNull
+    @NonNull
+    @Column(name = "password")
+    //Minimum eight characters, at least one uppercase letter, one lowercase letter and one number
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$")
+    private String password;
+}
