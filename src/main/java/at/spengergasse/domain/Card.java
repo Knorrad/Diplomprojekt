7cd707a4b75b25627c2527cfca8178a4bee403f0
Konrad Lysy <konrad.lysy@hotmail.com>
2018-01-09 11:13:33 +0100

    Add attributes to Card
    Signed-off-by: Konrad Lysy <konrad.lysy@hotmail.com>

--- src/main/java/at/spengergasse/domain/Card.java
+++ src/main/java/at/spengergasse/domain/Card.java
@@ -2,10 +2,15 @@ package at.spengergasse.domain;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
@@ -13,6 +18,23 @@ import javax.persistence.Table;
 @NoArgsConstructor
 @RequiredArgsConstructor
 public class Card extends BaseDomain<Card>{
+
+    @NonNull
+    @NotNull
+    @Column(name = "question")
+    private String question;
+
+
+    @NonNull
+    @NotNull
+    @Column(name = "correctAnswers")
+    List<String> correctAnswers = new ArrayList<>();
+
+
+    @NonNull
+    @NotNull
+    @Column(name = "wrongAnswers")
+    List<String> wrongAnswers = new ArrayList<>();
 
 
     @Override
