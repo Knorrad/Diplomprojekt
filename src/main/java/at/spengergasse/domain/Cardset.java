323479c96a708779b8f2ae01e1239070995bcf5c
Konrad Lysy <konrad.lysy@hotmail.com>
2018-01-09 15:28:57 +0100

    Add attributes in Cardset
    Signed-off-by: Konrad Lysy <konrad.lysy@hotmail.com>

--- src/main/java/at/spengergasse/domain/Cardset.java
+++ src/main/java/at/spengergasse/domain/Cardset.java
@@ -1,13 +1,58 @@
 package at.spengergasse.domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.NoRepositoryBean;
 
-public class Cardset extends BaseDomain<T> {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "Cardset")
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Cardset extends BaseDomain<Cardset> {
+
+    @NonNull
+    @NotNull
+    @Column(name = "category")
+    private Category category;
+
+
+    @NonNull
+    @NotNull
+    @Column(name = "name")
+    private String name;
+
+
+    @NonNull
+    @NotNull
+    @Column(name = "creator")
+    private User creator;
+
+
+    @NonNull
+    @NotNull
+    @Column (name = "cardlist")
+    List<Card> cardList = new ArrayList<>();
+
+
+
+
+
 
 
 
 
     @Override
-    public int compareTo(T o) {
+    public int compareTo(Cardset o) {
         return 0;
     }
 }
