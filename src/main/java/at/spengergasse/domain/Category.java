b435521fd37ce676ad15d28427bfc4d29e539f73
Konrad Lysy <konrad.lysy@hotmail.com>
2018-01-09 15:28:07 +0100

    Add attributes in Category
    Signed-off-by: Konrad Lysy <konrad.lysy@hotmail.com>

--- src/main/java/at/spengergasse/domain/Category.java
+++ src/main/java/at/spengergasse/domain/Category.java
@@ -2,10 +2,13 @@ package at.spengergasse.domain;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 @Data
 @Entity
@@ -14,6 +17,10 @@ import javax.persistence.Table;
 @RequiredArgsConstructor
 public class Category extends BaseDomain<Category>{
 
+    @NonNull
+    @NotNull
+    @Column(name = "name")
+    private String name;
 
 
     @Override
