4db33c75dd012081645a3df488ed1b3065683c78
Konrad Lysy <konrad.lysy@hotmail.com>
2018-01-09 15:37:59 +0100

    Add attributes in Group
    Signed-off-by: Konrad Lysy <konrad.lysy@hotmail.com>

--- src/main/java/at/spengergasse/domain/Group.java
+++ src/main/java/at/spengergasse/domain/Group.java
@@ -1,6 +1,35 @@
 package at.spengergasse.domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "Group")
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class Group extends BaseDomain<Group> {
+
+
+    @NonNull
+    @NotNull
+    @Column(name = "userlist")
+    List<User> userList = new ArrayList<>();
+
+
+    @NonNull
+    @NotNull
+    @Column(name = "cardsetlist")
+    List<Cardset> cardsetList = new ArrayList<>();
 
 
     @Override
