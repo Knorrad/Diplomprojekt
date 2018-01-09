b4747da97a8c98f5f04d8382033cd7611aeef39d
Konrad Lysy <lys16261@spengergasse.at>
2018-01-09 10:12:12 +0100

    Setup Spring project
    Signed-off-by: Konrad Lysy <konrad.lysy@hotmail.com>

---
+++ src/main/java/at/spengergasse/DiplomprojektApplication.java
@@ -0,0 +1,12 @@
+package at.spengergasse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomprojektApplication.class, args);
+	}
+}
